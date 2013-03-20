5d72423b3d925531bfa313f9d4a31551f96127fa
Carlos Celma <kaayro@gmail.com>
2013-03-19 21:22:40 -0600

    asd

--- js/vas-fnc.js
+++ js/vas-fnc.js
@@ -2,7 +2,9 @@ document.addEventListener("deviceready", function(){
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){//Permiso al sistema de archivos
 		//Permiso Concedido
 		fileSystem.root.getDirectory("vasFolder/projects", {create: true, exclusive: false}, function(){//Crea directorio en matriz
-			alert('hecho');
+			fileSystem.root.getFile("vasFolder/projects/prueba.html", {create: true, exclusive: false}, function(){
+				alert('hecho');
+			}, onFSFail);
 		}, onFSFail);
 	}, onFSFail);
 	
